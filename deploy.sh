97962da3985d339afddbc626d94be45b7814e1c2
Cephas Lin <cephalin@microsoft.com>
2017-10-20 13:34:41 +0200

    remove npm install --only=dev

--- deploy.sh
+++ deploy.sh
@@ -113,7 +113,6 @@ selectNodeVersion
 if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
   eval $NPM_CMD install --production
-  eval $NPM_CMD install --only=dev
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
 fi
