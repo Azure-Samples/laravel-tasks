cca114f46388c313d545e8d2f04924d6d3519061
ctc.Sebi <sebi@ctc-gaming.com>
2018-10-21 12:36:26 +0200

    Update env.example to use mysql8 connection and add mysql8-connection to config
    Fixes #3
    @see: https://github.com/laravel/framework/pull/23948

--- config/database.php
+++ config/database.php
@@ -66,6 +66,29 @@ return [
             'sslmode' => 'prefer',
         ],
 
+        'mysql8' => [
+            'driver'      => 'mysql',
+            'host'        => env( 'DB_HOST', '127.0.0.1' ),
+            'port'        => env( 'DB_PORT', '3306' ),
+            'database'    => env( 'DB_DATABASE', 'forge' ),
+            'username'    => env( 'DB_USERNAME', 'forge' ),
+            'password'    => env( 'DB_PASSWORD', '' ),
+            'unix_socket' => env( 'DB_SOCKET', '' ),
+            'charset'     => 'utf8mb4',
+            'collation'   => 'utf8mb4_unicode_ci',
+            'prefix'      => '',
+            'strict'      => true,
+            'engine'      => null,
+            'modes'       => [
+                'ONLY_FULL_GROUP_BY',
+                'STRICT_TRANS_TABLES',
+                'NO_ZERO_IN_DATE',
+                'NO_ZERO_DATE',
+                'ERROR_FOR_DIVISION_BY_ZERO',
+                'NO_ENGINE_SUBSTITUTION',
+            ],
+        ],
+
     ],
 
     /*
