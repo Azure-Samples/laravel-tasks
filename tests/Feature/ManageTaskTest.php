d62e365012402ef0135e481e5716cd74cd18a83c
Nafies Luthfi <nafiesl@gmail.com>
2018-10-21 20:43:29 +0800

    Add task deletion test

--- tests/Feature/ManageTaskTest.php
+++ tests/Feature/ManageTaskTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Task;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
@@ -20,6 +21,27 @@ class ManageTaskTest extends TestCase
 
         $this->assertDatabaseHas('tasks', [
             'name' => 'My new task',
+        ]);
+    }
+
+    /** @test */
+    public function user_can_delete_existing_task()
+    {
+        $task = new Task;
+        $task->name = 'Task to be deleted';
+        $task->save();
+
+        $this->assertDatabaseHas('tasks', [
+            'name' => 'Task to be deleted',
+        ]);
+
+        $response = $this->delete("/task/{$task->id}");
+
+        $response->assertRedirect('/');
+
+        $this->assertDatabaseMissing('tasks', [
+            'id'   => $task->id,
+            'name' => 'Task to be deleted',
         ]);
     }
 }
