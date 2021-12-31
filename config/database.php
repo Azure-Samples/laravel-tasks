2a74dfc718f4ad6f95031b0f53fe7f3dce5f68f1
dika <fauzan.dika@ecomindo.com>
2021-12-31 14:49:38 +0700

    update

--- config/database.php
+++ config/database.php
@@ -40,7 +40,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
