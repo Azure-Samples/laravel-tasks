1590e8c4597f5abf014f8d673bb5196c83795db3
v-daca <106557021+v-daca@users.noreply.github.com>
2022-12-12 17:53:59 -0600

    Update database.php
    Adding modes array to fix MySQL issue.

--- config/database.php
+++ config/database.php
@@ -58,6 +58,14 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            'modes'       => [
+                'ONLY_FULL_GROUP_BY',
+                'STRICT_TRANS_TABLES',
+                'NO_ZERO_IN_DATE',
+                'NO_ZERO_DATE',
+                'ERROR_FOR_DIVISION_BY_ZERO',
+                'NO_ENGINE_SUBSTITUTION',
+            ],
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
