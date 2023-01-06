9542c2959f4f811fea03501530a1143980ab8b9e
lai.yan.shen <lai.yan.shen@tecotec.co.jp>
2023-01-07 00:29:18 +0900

    add header

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: *');
 /*
 |--------------------------------------------------------------------------
 | Create The Application
