dfed3343b24c10eefd089c0ac8be03ec91362333
erikhhansen <125272115+erikhhansen@users.noreply.github.com>
2023-02-18 21:10:31 +0200

    qaz

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -29,7 +29,7 @@
                         <div class="form-group">
                             <div class="col-sm-offset-3 col-sm-6">
                                 <button type="submit" class="btn btn-default">
-                                    <i class="fa fa-btn fa-plus"></i>Add Task
+                                    <i class="fa fa-btn fa-plus"></i>Add Tas
                                 </button>
                             </div>
                         </div>
