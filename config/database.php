e24bd42aa22d077d28e1b8f26884c7503f766d19
Juassem Junio Do Vale Ayache <98559582+JuassemJunio@users.noreply.github.com>
2023-10-25 23:37:48 -0300

    Não consegui

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', 'azure-01-server.mysql.database.azure.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'azure-01-databas'),
+            'database' => env('DB_DATABASE', 'azure-01-database'),
             'username' => env('DB_USERNAME', 'wofzdhgaqz'),
             'password' => env('DB_PASSWORD', 'Q531318YM2B2IPM5$'),
             'unix_socket' => env('DB_SOCKET', ''),
