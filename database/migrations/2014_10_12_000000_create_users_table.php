b80a19a8ec1e3c94380717d5fce728a6e760f90e
Harikrishna902 <46101642+Harikrishna902@users.noreply.github.com>
2023-10-30 15:40:47 +0530

    changed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,67 +1,33 @@
 <?php
 
-// use Illuminate\Database\Migrations\Migration;
-// use Illuminate\Database\Schema\Blueprint;
-// use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-// return new class extends Migration
-// {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
-    // public function up(): void
-    // {
-    //     Schema::create('users', function (Blueprint $table) {
-    //         $table->id();
-    //         $table->string('name');
-    //         $table->string('email')->unique();
-    //         $table->timestamp('email_verified_at')->nullable();
-    //         $table->string('password');
-    //         $table->rememberToken();
-    //         $table->timestamps();
-    //     });
-    // }
+    public function up(): void
+    {   
+        Schema::dropIfExists('users');
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
 
     /**
      * Reverse the migrations.
      */
-    // public function down(): void
-    // {
-    //     Schema::dropIfExists('users');
-    // }
-//};
-
-
-
-
-
-
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
-
-class CreateUserTable extends Migration
-{
-
-    public function up()
-    {
-        Schema::dropIfExists('users');
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('role_id')->unsigned();
-            $table->string('name');
-            $table->string('email',50)->unique();
-            $table->string('username');
-            $table->string('password');
-            $table->boolean('active');
-            $table->rememberToken();
-            $table->timestamps();
-
-        });
-    }
-
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('users');
     }
-}
+};
