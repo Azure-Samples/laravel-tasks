ebac1702b7af0aac716c5e398bbaa1b3ebb1a63b
johnlee-sumo <john.lee@sumb.com.au>
2024-07-01 16:53:46 +0800

    already exists error

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::dropIfExists('password_reset_tokens');
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
