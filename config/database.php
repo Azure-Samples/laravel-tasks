1a0bd69da1295c8a9be712011d65d9f04deb8252
Anmol9295 <140419297+Anmol9295@users.noreply.github.com>
2024-10-15 11:10:50 -0600

    CHANGEAR FINAL

--- config/database.php
+++ config/database.php
@@ -140,6 +140,7 @@ return [
 
         'cache' => [
             'scheme' => 'tls',
+            
             'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('AZURE_REDIS_PASSWORD'),
