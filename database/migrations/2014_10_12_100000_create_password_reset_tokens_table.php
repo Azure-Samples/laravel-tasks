8f8060c7b6d3036d06a78e924e49fefa6bca20fa
Randy <13327816+randy-jin@users.noreply.github.com>
2025-02-05 00:21:04 -0700

    delete primary key

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -11,10 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
-        
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('email');
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
