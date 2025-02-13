2e98e97e4335d8a2f150707059e789588a0c5237
Adrian Cruz <152443965+iiancz@users.noreply.github.com>
2025-02-12 22:07:51 -0700

    updated again

--- config/database.php
+++ config/database.php
@@ -48,9 +48,9 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('AZURE_MYSQL_HOST', '127.0.0.1'),
             'port' => env('AZURE_MYSQL_PORT', '3306'),
-            'database' => env('AZURE_MYSQL_DBNAME', 'msdocs-laravel-mysql-crz-database'), // updated value
-            'username' => env('AZURE_MYSQL_USERNAME', 'cavsosynrp'), // updated value
-            'password' => env('AZURE_MYSQL_PASSWORD', 'nP$KM7Xlm3Y5$raO'), // updated this value
+            'database' => env('AZURE_MYSQL_DBNAME', 'forge'),
+            'username' => env('AZURE_MYSQL_USERNAME', 'forge'),
+            'password' => env('AZURE_MYSQL_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -130,20 +130,20 @@ return [
 
         'default' => [
             'url' => env('REDIS_URL'),
-            'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
-            'password' => env('AZURE_REDIS_PASSWORD'),
-            'port' => env('AZURE_REDIS_PORT', '6379'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
         ],
 
         'cache' => [
             'scheme' => 'tls',
             'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
+            'password' => env('AZURE_REDIS_PASSWORD'),
+            'port' => env('AZURE_REDIS_PORT', '6379'),
             'database' => env('AZURE_REDIS_DATABASE', '1'),
         ],
 
